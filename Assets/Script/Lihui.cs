e563e9c6507592eda1383564e68d43fae9269bb3
MillyYe <milly.ye@cssinco.cn>
2014-08-01 15:09:57 +0800

    add files

---
+++ Assets/Script/Lihui.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Lihui  {
+
+	[DllImport("__Internal")]
+	private static extern void viewDidLoad();
+
+	public static void weibo ()
+	{
+		
+		if (Application.platform != RuntimePlatform.OSXEditor) 
+		{
+		
+			viewDidLoad();
+		}
+	}
+
+}
